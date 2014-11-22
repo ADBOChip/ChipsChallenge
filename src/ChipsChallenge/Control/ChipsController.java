e72a724284603993858dacc5283ab4071d5a830a
bimobebebe <bimo_bartholomeus@yahoo.co.id>
2014-11-22 17:17:39 +0700

    JavaDoc

--- src/ChipsChallenge/Control/ChipsController.java
+++ src/ChipsChallenge/Control/ChipsController.java
@@ -14,35 +14,58 @@ import ChipsChallenge.grafficStorage.ChipImageDrawer;
 public class ChipsController {
     ChipImageDrawer GUI;
     private Board b,c;
-
+    
+    /**
+     * Konstruktor
+     */
     public ChipsController() {
         b = new Board();
         this.b.newGame();
-        c=new Board();
+        c = new Board();
     }
 
+    /**
+     * Menggerakan Chip keatas
+     * Memanggil method playerMoveUp dari kelas Board
+     */
     public void moveChipUp() {
         this.b.playerMoveUp();
         this.b.repaintMap();
     }
     
+    /**
+     * Menggerakan Chip kekanan
+     * Memanggil method playerMoveRight dari kelas Board
+     */
     public void moveChipRight() {
         b.playerMoveRight();
         this.b.repaintMap();
 
     }
-
+    
+    /**
+     * Menggerakan Chip kebawah
+     * Memanggil method playerMoveDown dari kelas Board
+     */
     public void moveChipDown() {
         this.b.playerMoveDown();
         this.b.repaintMap();
     }
 
+    /**
+     * Menggerakan Chip kekiri
+     * Memanggil method playerMoveLeft dari kelas Board
+     */
     public void moveChipLeft() {
         this.b.playerMoveLeft();
         this.b.repaintMap();
     }
 
-
+    /**
+     * Mengetahui status kehidupan Player. memanggil method isAlive dari Board
+     * @return true jika player masih hidup
+     * @return false jika player mati
+     */
     public boolean chipIsAlive() {
         return this.b.isAlive();
     }
@@ -50,7 +73,13 @@ public class ChipsController {
     public int pjgBoard() {
         return this.b.panjangTile();
     }
-
+    
+    /**
+     * Mengembalikan nilai info pada Tile tertentu
+     * @param x : koordinat x Tile
+     * @param y : koordinat y Tile
+     * @return info pada tile
+     */
     public char getTileInfo(int x, int y) {
         return this.b.getTileInfo(x, y);
     }
